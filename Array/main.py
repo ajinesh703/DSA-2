77351e1de96d4bb26b62f4db0528a9e586c87b22
Ajinesh Pratap Singh <152050389+ajinesh703@users.noreply.github.com>
2024-12-21 19:08:11 +0530

    Create main.py

---
+++ Array/main.py
@@ -0,0 +1,10 @@
+def plusOne(digits):
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits
+        digits[i] = 0
+    return [1] + digits
+
+# Example
+print(plusOne([1, 2, 3]))  # Output: [1, 2, 4]
