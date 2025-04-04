08dd9cd77074e1e401a59d4c4052c39f3026a3d5
Ajinesh Pratap Singh <152050389+ajinesh703@users.noreply.github.com>
2025-04-04 18:02:51 +0530

    Create Merge Intervals.py

---
+++ Array/Merge Intervals.py
@@ -0,0 +1,9 @@
+def merge(intervals):
+    intervals.sort()  # Sort by start time
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
+        else:
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    return merged
