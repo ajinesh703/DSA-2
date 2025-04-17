1863f7bd74b528226de93e86bc7630cde717eed3
Ajinesh Pratap Singh <152050389+ajinesh703@users.noreply.github.com>
2025-04-17 19:35:03 +0530

    Create EqualPairs.py

---
+++ Hash Table/EqualPairs.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+def equalPairs(grid):
+    n = len(grid)
+    row_count = Counter(tuple(row) for row in grid)
+    col_count = Counter(tuple(grid[i][j] for i in range(n)) for j in range(n))
+    return sum(row_count[row] * col_count[row] for row in row_count)
